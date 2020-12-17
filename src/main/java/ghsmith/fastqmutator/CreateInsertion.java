e7a8df526b3f5e14dc0f812fd0703f9ff7b92bde
ghsmith <geoffrey.hughes.smith@gmail.com>
2020-12-17 13:32:23 -0500

    .

--- src/main/java/ghsmith/fastqmutator/CreateInsertion.java
+++ src/main/java/ghsmith/fastqmutator/CreateInsertion.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -38,6 +40,11 @@ public class CreateInsertion {
                     System.out.println(String.format("%s : rejected (%s)", samRecord.qname, "CIGAR = " + samRecord.cigar));
                     continue;
                 }
+                if(samRecord.insert == null) {
+                    System.out.println(String.format("%s : rejected (%s)", samRecord.qname, "no applicable insert found"));
+                    continue;
+                }
+                assert samRecord.simpleLength() == samRecord.seq.length();
                 System.out.println(String.format("%s : accepted for insert %s", samRecord.qname, samRecord.insert));
                 samRecords.add(samRecord);
             }
@@ -60,6 +67,22 @@ public class CreateInsertion {
                             continue;
                         }
                         System.out.println(String.format("%s : match in %s", samRecord.qname, fastqFileName));
+                        assert fastqRecord[1].length() == samRecord.simpleLength();
+                        String newSequence = String.format("%s%s%s",
+                            fastqRecord[1].substring(0, samRecord.insert.position - samRecord.pos),
+                            samRecord.isReverseComplement() ? reverseComplement(samRecord.insert.sequence) : samRecord.insert.sequence,
+                            fastqRecord[1].substring(samRecord.insert.position)
+                        );
+                        while(newSequence.length() > samRecord.simpleLength()) {
+                            if(Math.random() < 0.5) {
+                                newSequence = newSequence.substring(1);
+                            }
+                            else {
+                                newSequence = newSequence.substring(0, newSequence.length() - 1);
+                            }
+                        }
+                        System.out.println(String.format("...old: %s", fastqRecord[1]));
+                        System.out.println(String.format("...new: %s", newSequence));
                     }
                 }
             }
@@ -67,6 +90,22 @@ public class CreateInsertion {
             fastqWriter.close();
         }
         
+    }
+
+    public static Map<Character, Character> complementMap = new HashMap<>();
+    static {
+        complementMap.put('A', 'T');
+        complementMap.put('C', 'G');
+        complementMap.put('G', 'C');
+        complementMap.put('T', 'A');
+    }
+    public static String reverseComplement(String sequence) {
+        StringBuilder reverseComplementSequence = new StringBuilder();
+        for(int x = sequence.length() - 1; x >= 0; x++) {
+            assert complementMap.keySet().contains(sequence.charAt(x));
+            reverseComplementSequence.append(complementMap.get(sequence.charAt(x)));
+        }
+        return reverseComplementSequence.toString();
     }
     
 }
--- src/main/java/ghsmith/fastqmutator/Insert.java
+++ src/main/java/ghsmith/fastqmutator/Insert.java
@@ -8,17 +8,17 @@ public class Insert {
     
     public String chromosome;
     public Integer position;
-    public String insertionSequence;
+    public String sequence;
 
-    public Insert(String chromosome, Integer position, String insertionSequence) {
+    public Insert(String chromosome, Integer position, String sequence) {
         this.chromosome = chromosome;
         this.position = position;
-        this.insertionSequence = insertionSequence;
+        this.sequence = sequence;
     }
 
     @Override
     public String toString() {
-        return "{" + chromosome + ", " + position + ", " + insertionSequence + '}';
+        return "{" + chromosome + ", " + position + ", " + sequence + '}';
     }
     
 }
